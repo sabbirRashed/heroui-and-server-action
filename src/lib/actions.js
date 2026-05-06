3dd926af4647ddf98a4fa85befca7e7afceefebb
sabbirRashed <srsabbirrahman12@gmail.com>
2026-05-06 14:22:29 +0600

    post new task

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,7 +1,24 @@
+import { revalidatePath } from "next/cache";
+import { postTask } from "./task";
 
-export const createATask = async(formData) =>{
+export const createATask = async (formData) => {
     "use server"
 
-    const name = formData.get('name');
-    console.log("adding a task with name", formData);
+    // const title = formData.get("title");
+    // const description = formData.get("description");
+    // const priority = formData.get("priority");
+    // const status = formData.get("status");
+    // const assignTo = formData.get("assignTo");
+
+    // const newTask = {title, description, priority, status, assignTo};
+
+    const newTask = Object.fromEntries(formData.entries());
+
+    console.log("adding a task with name", newTask);
+    const res = await postTask(newTask);
+
+    if (res.ok) {
+        revalidatePath('/tasks')
+    }
+    return res;
 }
--- src/lib/task.js
+++ src/lib/task.js
@@ -1,5 +1,15 @@
-import tasks from '../data/task.json';
+import tasksData from '../data/task.json';
 
-export const getTasks = async() =>{
-    return tasks;
+export const getTasks = async () => {
+    return tasksData;
+}
+
+export const postTask = async (newTask) => {
+    const tasks = tasksData.tasks
+    
+    newTask.id = tasks.length + 1;
+    // tasks.push(newTask);
+    tasks.unshift(newTask);
+
+    return { ok: true, message: "task added successfully" }
 }
